996fa5790b638956fdf3834a29f603087779c259
Linet Oliech <linetaoliech@gmail.com>
2023-03-26 13:06:09 +0300

    completed assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,29 @@
 class ItemsController < ApplicationController
 
   def index
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+  else
     items = Item.all
+  end
     render json: items, include: :user
+  end
+  
+  def show
+    item = Item.find(params[:id])
+    render json: item, include: :user
+  end
+
+  def create
+    item = Item.create!(items_params)
+    render json: item, include: :user, status: 201
+  end
+
+  private
+
+  def items_params
+    params.permit(:name, :description, :price, :user_id)
   end
 
 end
